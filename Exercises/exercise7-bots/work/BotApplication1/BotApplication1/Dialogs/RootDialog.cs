644ab56786e2f1897da01551b98dac389cecbbb5
bibliothek <thaller.martin@gmail.com>
2018-01-10 16:30:35 +0100

    exercise 7

---
+++ Exercises/exercise7-bots/work/BotApplication1/BotApplication1/Dialogs/RootDialog.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using BotApplication1.Data;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace BotApplication1.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as Activity;
+
+            if (activity.Text.Contains("order"))
+            {
+                context.Call(new StockItemSelectionDialog(), OnStockItemReceivedAsync);
+            }
+        }
+
+        private async Task OnStockItemReceivedAsync(IDialogContext context, IAwaitable<Stockitem> result)
+        {
+            await context.PostAsync("Thank you!");
+
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
---
+++ Exercises/exercise7-bots/work/BotApplication1/BotApplication1/Dialogs/StockItemSelectionDialog.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using BotApplication1.Data;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace BotApplication1.Dialogs
+{
+    [Serializable]
+    public class StockItemSelectionDialog : IDialog<Stockitem>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+
+            var dialog = new PromptDialog.PromptString("What do you want to order?", "Please tell me the name of the product.", 3);
+
+            context.Call(dialog, OnStockitemNameReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+        private async Task OnStockitemNameReceivedAsync(IDialogContext context, IAwaitable<string> result)
+        {
+            var stockitemName = await result;
+
+            // send typing message
+            var typingMessage = context.MakeMessage();
+            typingMessage.Type = ActivityTypes.Typing;
+
+            await context.PostAsync(typingMessage);
+
+            await Task.Delay(3000);
+
+            // get results
+            var dataManager = new DataManager();
+            var stockItems = await dataManager.GetStockitemsAsync(stockitemName);
+
+            if (stockItems.Count == 0)
+            {
+                var dialog = new PromptDialog.PromptString("Sorry, we didn't find this article. What is its name?", "Sorry. What product did you mean?", 3);
+
+                context.Call(dialog, OnStockitemNameReceivedAsync);
+            }
+            else if (stockItems.Count == 1)
+            {
+                await SendOrderConfirmationAsync(context, stockItems.First());
+            }
+            else
+            {
+                var dialog = new PromptDialog.PromptChoice<Stockitem>(stockItems, "Please select your product.", "Please select.", 3);
+
+                context.Call(dialog, OnStockItemReceivedAsync);
+            }
+        }
+
+        private async Task OnStockItemReceivedAsync(IDialogContext context, IAwaitable<Stockitem> result)
+        {
+            var stockitem = await result;
+
+            await SendOrderConfirmationAsync(context, stockitem);
+        }
+
+        private async Task SendOrderConfirmationAsync(IDialogContext context, Stockitem stockitem)
+        {
+            var card = new HeroCard()
+            {
+                Title = stockitem.StockItemName,
+                Text = $"Buy now for {stockitem.RecommendedRetailPrice} $!",
+                Images = new List<CardImage>()
+        {
+            new CardImage()
+            {
+                Url = stockitem.ImageUrl,
+                Tap = new CardAction(ActionTypes.OpenUrl, "Open", null, "https://docs.microsoft.com/en-us/sql/sample/world-wide-importers")
+            }
+        }
+            };
+
+            var responseMessage = context.MakeMessage();
+            responseMessage.Attachments.Add(card.ToAttachment());
+
+            await context.PostAsync(responseMessage);
+
+            context.Done(stockitem);
+        }
+    }
+}
